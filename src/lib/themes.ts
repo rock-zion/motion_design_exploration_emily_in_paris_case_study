fd780f5d4783a1a5ff55d140a0d31fa4c53341de
rock-zion <rock_zion@outlook.com>
2026-01-28 01:29:48 +0100

    updates themes data

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -17,6 +17,39 @@ export const themes = {
     unlockExperience: {
       title: 'Explore at your own pace',
       ctaBtnText: 'View Experiences',
+      cta: 'Unlock The Emily in Paris Experience',
+
+      cursorTrailImages: [
+        '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+        '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+        '/images/Macaron_making_filling_the_macarons.avif',
+        '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+        '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+        '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+        '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+        '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+        '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+      ],
+
+      videoSection: {
+        video: '/video/paris-hero-b-video.mp4',
+        marqueeText: 'Discover Experiences',
+        marqueeSticker: '/images/stickers/sticker-montmartre-tour.avif',
+
+        main1: {
+          title: 'MONTMARTRE TOUR',
+          video: '/video/montmarte-b-transcode.mp4',
+        },
+        main2: {
+          title: 'CHAMPAGNE SEINE CRUISE',
+          video: '/video/croissants-a-transcode.mp4',
+        },
+
+        main3: {
+          title: 'WINE & CHEESE TASTING',
+          video: '/video/wine-tasting-b-transcode.mp4',
+        },
+      },
     },
   },
   rome: {
@@ -37,6 +70,36 @@ export const themes = {
       title: 'Explore at your own pace',
       ctaBtnText: 'View Experiences',
       cta: '<span>Unlock The Emily in<span>paris<span>Rome</span></span>Experience</span>',
+      cursorTrailImages: [
+        '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+        '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+        '/images/Macaron_making_filling_the_macarons.avif',
+        '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+        '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+        '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+        '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+        '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+        '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+      ],
+
+      videoSection: {
+        video: '/video/paris-hero-b-video.mp4',
+        marqueeText: 'Discover Experiences',
+        marqueeSticker: '/images/stickers/sticker-montmartre-tour.avif',
+        main1: {
+          title: 'MONTMARTRE TOUR',
+          video: '/video/montmarte-b-transcode.mp4',
+        },
+        main2: {
+          title: 'CHAMPAGNE SEINE CRUISE',
+          video: '/video/croissants-a-transcode.mp4',
+        },
+
+        main3: {
+          title: 'WINE & CHEESE TASTING',
+          video: '/video/wine-tasting-b-transcode.mp4',
+        },
+      },
     },
   },
 } as const;
