3b404966ad48599d291d85b0f946e523d951fbf9
rock-zion <rock_zion@outlook.com>
2026-01-28 15:05:50 +0100

    comment out lenis till i figure out what the hell is going on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,47 @@
+'use client';
+
 import './globals.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
+// import Lenis from 'lenis';
+// import { useEffect } from 'react';
+// import { ScrollTrigger } from 'gsap/all';
+// import { gsap } from 'gsap';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type RootLayoutProps = {
   children: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 4,
+  //     easing: (t: number): number => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  //     smoothWheel: true,
+  //   });
+
+  //   lenis.on('scroll', ScrollTrigger.update);
+
+  //   gsap.ticker.add(time => {
+  //     lenis.raf(time * 1000);
+  //   });
+
+  //   gsap.ticker.lagSmoothing(0);
+
+  //   function raf(time: number): void {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+
+  //   requestAnimationFrame(raf);
+
+  //   return () => {
+  //     gsap.ticker.remove(lenis.raf);
+  //     lenis.destroy();
+  //   };
+  // }, []);
+
   return (
-    <html lang='en'>
+    <html suppressHydrationWarning lang='en'>
       <ThemeProvider>{children}</ThemeProvider>
     </html>
   );
