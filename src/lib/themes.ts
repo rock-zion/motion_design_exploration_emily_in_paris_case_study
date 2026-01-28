768d081c324f96390a411b35ca106cdbc8c02e9e
rock-zion <rock_zion@outlook.com>
2026-01-29 00:20:45 +0100

    no message

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -20,15 +20,39 @@ export const themes = {
       cta: 'Unlock The Emily in Paris Experience',
 
       cursorTrailImages: [
-        '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
-        '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
-        '/images/Macaron_making_filling_the_macarons.avif',
-        '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
-        '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
-        '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
-        '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
-        '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
-        '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+        {
+          id: '1',
+          img: '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+        },
+        {
+          id: '2',
+          img: '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+        },
+        { id: '3', img: '/images/Macaron_making_filling_the_macarons.avif' },
+        {
+          id: '4',
+          img: '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+        },
+        {
+          id: '5',
+          img: '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+        },
+        {
+          id: '6',
+          img: '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+        },
+        {
+          id: '7',
+          img: '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+        },
+        {
+          id: '8',
+          img: '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+        },
+        {
+          id: '9',
+          img: '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+        },
       ],
 
       videoSection: {
@@ -80,6 +104,30 @@ export const themes = {
         },
       ],
     },
+
+    maequeeMadness: [
+      {
+        id: '1',
+        textA: 'Walking Tour',
+        stickerA: '/images/stickers/sticker-montmartre-tour.avif',
+        textB: 'Food Tour',
+        stickerB: '/images/stickers/sticker-food-tour.avif',
+      },
+      {
+        id: '2',
+        textA: 'Cheese',
+        stickerA: '/images/stickers/sticker-wine-cheese-tasting.avif',
+        textB: 'Wine',
+        stickerB: '/images/stickers/sticker-champagne-seine-cruise.avif',
+      },
+      {
+        id: '3',
+        textA: 'MACARON',
+        stickerA: '/images/stickers/sticker-macaron-making-workshop.avif',
+        textB: 'CROISSANT',
+        stickerB: '/images/stickers/sticker-croissant-making-workshop.avif',
+      },
+    ],
   },
   rome: {
     name: 'Rome',
@@ -100,15 +148,39 @@ export const themes = {
       ctaBtnText: 'View Experiences',
       cta: '<span>Unlock The Emily in<span>paris<span>Rome</span></span>Experience</span>',
       cursorTrailImages: [
-        '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
-        '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
-        '/images/Macaron_making_filling_the_macarons.avif',
-        '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
-        '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
-        '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
-        '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
-        '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
-        '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+        {
+          id: '1',
+          img: '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+        },
+        {
+          id: '2',
+          img: '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+        },
+        { id: '3', img: '/images/Macaron_making_filling_the_macarons.avif' },
+        {
+          id: '4',
+          img: '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+        },
+        {
+          id: '5',
+          img: '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+        },
+        {
+          id: '6',
+          img: '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+        },
+        {
+          id: '7',
+          img: '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+        },
+        {
+          id: '8',
+          img: '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+        },
+        {
+          id: '9',
+          img: '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+        },
       ],
 
       videoSection: {
@@ -160,6 +232,30 @@ export const themes = {
         },
       ],
     },
+
+    maequeeMadness: [
+      {
+        id: '1',
+        textA: 'Walking Tour',
+        stickerA: '/images/stickers/sticker-montmartre-tour.avif',
+        textB: 'Food Tour',
+        stickerB: '/images/stickers/sticker-food-tour.avif',
+      },
+      {
+        id: '2',
+        textA: 'Cheese',
+        stickerA: '/images/stickers/sticker-wine-cheese-tasting.avif',
+        textB: 'Wine',
+        stickerB: '/images/stickers/sticker-champagne-seine-cruise.avif',
+      },
+      {
+        id: '3',
+        textA: 'MACARON',
+        stickerA: '/images/stickers/sticker-macaron-making-workshop.avif',
+        textB: 'public/images/stickers/sticker-croissant-making-workshop.avif',
+        stickerB: 'CROISSANT',
+      },
+    ],
   },
 } as const;
 
