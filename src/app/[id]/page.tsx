7c33772d0e6827f9c415175bd6cc05c6df801488
rock-zion <rock_zion@outlook.com>
2026-01-30 12:45:19 +0100

    remove mouse tracking code from "/city" page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,38 +4,19 @@ import { useRef } from 'react';
 import Hero from './Hero';
 import Navbar, { type INavbarHandle } from '@/components/navbar/Navbar';
 import Explore from './Explore';
-import { useGSAP } from '@gsap/react';
-import { gsap } from 'gsap';
 import VideoSection from './VideoSection';
 import Activities from './Activities';
 import MarqueeMadness from './MarqueeMadness';
 
 export const HomePage = () => {
   const navBarRef = useRef<INavbarHandle>(null);
-  const pageRef = useRef<HTMLElement>(null);
 
   const triggerMenuReveal = () => {
     navBarRef.current?.triggerMyFunction();
   };
 
-  useGSAP(
-    () => {
-      if (!pageRef.current) return;
-
-      const page = pageRef.current;
-      const handleMouseTrack = (e: MouseEvent) => {
-        const x = `${e.pageX - 10}px`;
-        const y = `${e.pageY - 10}px`;
-
-        gsap.to('.mouse-tracker', { x: x, y: y, duration: 0.5 });
-      };
-
-      page.addEventListener('mousemove', handleMouseTrack);
-    },
-    { scope: pageRef },
-  );
   return (
-    <main ref={pageRef}>
+    <main id='scrollArea'>
       <div className='absolute z-[900] mouse-tracker w-[8px] h-[8px] rounded-full bg-black'></div>
       <Navbar ref={navBarRef} />
       <Hero triggerMenuReveal={triggerMenuReveal} />
@@ -43,7 +24,6 @@ export const HomePage = () => {
       <VideoSection />
       <Activities />
       <MarqueeMadness />
-      <section className='h-screen'></section>
     </main>
   );
 };
