3c62b656b7ba8c0dec38d20b34b09cbb1c778cb4
rock-zion <rock_zion@outlook.com>
2026-01-30 12:38:09 +0100

    create separate rome and paris libs for easier management

---
+++ src/lib/rome.ts
@@ -0,0 +1,130 @@
+export const rome = {
+  name: 'Rome',
+  title: 'Rome by Emily',
+  subtitle:
+    "THE <strong>OFFICIAL</strong> TRAVEL BRAND OF NETFLIX'S EMILY IN PARIS",
+  heroVideo: '/video/paris-hero.mp4',
+  poster: '/images/paris-hero.mp4',
+  cta: 'Explore Rome',
+  description: 'Walk through ancient history and Italian charm',
+  hero: {
+    title: "Live the Rome you've only seen on screen",
+    subtitle:
+      'Created for people who dream of Rome and want to live it, not just visit',
+  },
+  unlockExperience: {
+    title: 'Explore at your own pace',
+    ctaBtnText: 'View Experiences',
+    cta: "<div className='relative unlock-experiences'>Unlock The <span className='inline-flex justify-center flex-wrap'><span className='inline-flex justify-around w-fit'><span>Emily in&nbsp;</span><img src='/images/6943dc7b97964d70a5d73e58_rome_text.svg' className='stylised-rome mobile' /></span><span className='struckout relative'>Paris<img src='/images/6943dc7b97964d70a5d73e58_rome_text.svg' className='absolute stylised-rome web' /></span></span> Experiences</div>",
+    cursorTrailImages: [
+      {
+        id: '1',
+        img: '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+      },
+      {
+        id: '2',
+        img: '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+      },
+      { id: '3', img: '/images/Macaron_making_filling_the_macarons.avif' },
+      {
+        id: '4',
+        img: '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+      },
+      {
+        id: '5',
+        img: '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+      },
+      {
+        id: '6',
+        img: '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+      },
+      {
+        id: '7',
+        img: '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+      },
+      {
+        id: '8',
+        img: '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+      },
+      {
+        id: '9',
+        img: '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+      },
+    ],
+
+    videoSection: {
+      video: '/video/paris-hero-b-video.mp4',
+      marqueeText: 'Discover Experiences',
+      marqueeSticker: '/images/stickers/sticker-montmartre-tour.avif',
+      subSections: {
+        main1: {
+          title: 'MONTMARTRE TOUR',
+          video: '/video/montmarte-b-transcode.mp4',
+        },
+        main2: {
+          title: 'CHAMPAGNE SEINE CRUISE',
+          video: '/video/croissants-a-transcode.mp4',
+        },
+
+        main3: {
+          title: 'WINE & CHEESE TASTING',
+          video: '/video/wine-tasting-b-transcode.mp4',
+        },
+      },
+    },
+
+    emilyActivities: [
+      {
+        id: '1',
+        video: '/video/food-tour-a-transcode.mp4',
+        type: 'WALKING TOURS',
+        title: '<span>Walk in the <br /> footsteps of Emily</span>',
+        duration: '2.5HOURS',
+        price: 'FROM52€',
+        category: 'tour',
+      },
+      {
+        id: '2',
+        video: '/video/croissants-a-transcode.mp4',
+        type: 'WORKSHOPS',
+        title: '<span>Croissant-Making <br /> Workshop</span>',
+        duration: '2.5HOURS',
+        price: 'FROM131€',
+        category: 'workshop',
+      },
+      {
+        id: '3',
+        video: '/video/macarons-a-transcode.mp4',
+        type: 'WORKSHOPS',
+        title: '<span>Macaron-Making <br /> Workshop</span>',
+        duration: '2.5HOURS',
+        price: 'FROM131€',
+        category: 'workshop',
+      },
+    ],
+  },
+
+  maequeeMadness: [
+    {
+      id: '1',
+      textA: 'Walking Tour',
+      stickerA: '/images/stickers/sticker-montmartre-tour.avif',
+      textB: 'Food Tour',
+      stickerB: '/images/stickers/sticker-food-tour.avif',
+    },
+    {
+      id: '2',
+      textA: 'Pizza',
+      stickerA: '/images/stickers/sticker-wine-cheese-tasting.avif',
+      textB: 'Pasta',
+      stickerB: '/images/stickers/sticker-champagne-seine-cruise.avif',
+    },
+    {
+      id: '3',
+      textA: 'APEROL',
+      stickerA: '/images/stickers/sticker-macaron-making-workshop.avif',
+      textB: 'RAVIOLI',
+      stickerB: '/images/stickers/sticker-croissant-making-workshop.avif',
+    },
+  ],
+};
