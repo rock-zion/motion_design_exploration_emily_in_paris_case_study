b0f41ec55c224e86f33b24128cd83d4f11c44738
rock-zion <rock_zion@outlook.com>
2026-02-07 17:51:14 +0100

    hijack scroll with lenis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@
 import './globals.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import useMouseTrack from '@/hooks/useMouseTrack';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
+import { ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap';
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -10,6 +14,34 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   useMouseTrack();
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 4,
+      easing: (t: number): number => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add(time => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    function raf(time: number): void {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      gsap.ticker.remove(lenis.raf);
+      lenis.destroy();
+    };
+  }, []);
 
   return (
     <html suppressHydrationWarning lang='en'>
