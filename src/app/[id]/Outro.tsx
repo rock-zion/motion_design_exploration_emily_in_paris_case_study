86b5cf2d54ed0ce66c40f0056b94b844a9cf8083
rock-zion <rock_zion@outlook.com>
2026-02-08 12:01:42 +0100

    improved outro responsiveness

--- src/app/[id]/Outro.tsx
+++ src/app/[id]/Outro.tsx
@@ -6,8 +6,11 @@ const Outro = () => {
   const { theme } = useTheme();
 
   return (
-    <div className='h-[60vh] flex flex-col items-center justify-center gap-8'>
-      {parse(theme.outro.text)}
+    <div className='h-[70vh] flex flex-col items-center justify-center gap-8'>
+      <div className='w-[70vw] max-w-[1260px] max-md:w-[95vw] flex justify-center'>
+        {parse(theme.outro.text)}
+      </div>
+
       <BubbleButton variant='out' size='xl'>
         View Experiences
       </BubbleButton>
