fcb69658086262c87103d2c99ac77093eaa7e3f2
rock-zion <rock_zion@outlook.com>
2026-02-08 05:43:24 +0100

    add responsiveness to footer

--- src/app/[id]/Footer.tsx
+++ src/app/[id]/Footer.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import BloomButton from '@/components/buttons/BloomButton';
 
 const Footer = () => {
   return (
-    <section className='sticky bottom-[-24rem] bg-(--background) h-screen z-[-1]'>
-      <div className='grid grid-cols-[60%_1fr_1fr] w-[95vw] mx-auto pt-[10vh]'>
-        <div className='flex flex-col'>
+    <section className='sticky bottom-[-24rem] bg-(--background) h-screen z-[-1] pointer-events-auto'>
+      <div className='grid grid-cols-[60%_1fr_1fr] w-[95vw] mx-auto pt-[10vh] max-md:flex flex-col'>
+        <div className='flex flex-col max-md:my-[24px]'>
           <img
             className=' w-[150px]'
             src={'/images/Netflix_Logo_RGB.png'}
             alt='netflix logo'
           />
-          <p className='font-montreal-book'>
+          <p className='font-montreal-book ml-4'>
             NO AFFILIATION WITH <br /> THE NETFLIX OR EMILY <br /> IN PARIS
             BRAND
           </p>
         </div>
         <div></div>
-        <div className='flex gap-x-[32px]'>
-          <div className='text-h6 font-mango'>CITY</div>
-          <div className='flex flex-col'>
-            <button>Paris</button>
-            <button>Rome</button>
+        <div className='flex gap-x-[32px] items-start max-md:flex-col max-md:ml-4'>
+          <div className='text-h5 font-mango bg-(--bg-brand-secondary) text-(--primitive-neutral-1000) px-[16px]'>
+            CITY
+          </div>
+          <div className='flex flex-col max-md:mt-[48px]'>
+            <BloomButton href='/paris'>Paris</BloomButton>
+            <BloomButton href='/rome'>Rome</BloomButton>
           </div>
         </div>
       </div>
       <p className='text-display font-rumble mt-[2vh] w-full text-center'>
         Paris By Emily
       </p>
-      <p className='font-montreal-book text-center'>
-        NO AFFILIATION WITH THE NETFLIX OR EMILY IN PARIS BRAND I BUILD THIS
-        LANDING PAGE SOLEY FOR THE PURPOSE OF DEMONSTRATING A SKILL
+      <p className='font-montreal-book text-center uppercase'>
+        No affiliation with Netflix or Emily in Paris. This landing page was
+        created solely for skill demonstration purposes.
       </p>
     </section>
   );
