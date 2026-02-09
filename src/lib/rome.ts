1289d8615c8791a4edc5a2f0302828a83ad4433d
rock-zion <rock_zion@outlook.com>
2026-02-09 10:56:41 +0100

    serve content with cdn

--- src/lib/rome.ts
+++ src/lib/rome.ts
@@ -3,8 +3,10 @@ export const rome = {
   title: 'Rome by Emily',
   subtitle:
     "THE <strong>OFFICIAL</strong> TRAVEL BRAND OF NETFLIX'S EMILY IN PARIS",
-  heroVideo: '/video/paris-hero.mp4',
-  poster: '/images/paris-hero.mp4',
+  heroVideo:
+    'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583238/emily_in_paris_motion_design_exploration/videos/paris-hero_ennfrl.mp4',
+  poster:
+    'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581105/emily_in_paris_motion_design_exploration/images/hero_poster_h1xlbt.avif',
   cta: 'Explore Rome',
   description: 'Walk through ancient history and Italian charm',
   hero: {
@@ -19,60 +21,72 @@ export const rome = {
     cursorTrailImages: [
       {
         id: '1',
-        img: '/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581191/emily_in_paris_motion_design_exploration/images/Walking_Tour_guests_holding_pastry_like_emily_in_paris-p-800_wbizgz.webp',
       },
       {
         id: '2',
-        img: '/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581105/emily_in_paris_motion_design_exploration/images/Macaron_making_guest_pouring_the_mix_to_pipe_the_macaron-p-800_e0pbxl.webp',
       },
-      { id: '3', img: '/images/Macaron_making_filling_the_macarons.avif' },
+      {
+        id: '3',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581104/emily_in_paris_motion_design_exploration/images/Macaron_making_filling_the_macarons_dvxvg8.avif',
+      },
       {
         id: '4',
-        img: '/images/Walking_Tour_Group_Photo_outside_Galerie-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581191/emily_in_paris_motion_design_exploration/images/Walking_Tour_Group_Photo_outside_Galerie-p-800_qnnqhx.webp',
       },
       {
         id: '5',
-        img: '/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581106/emily_in_paris_motion_design_exploration/images/Macaron_making_tais_taking_a_picture_of_2_guests_with_the_macaron_making_class_props-p-800_sd3eqy.webp',
       },
       {
         id: '6',
-        img: '/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581105/emily_in_paris_motion_design_exploration/images/Macaron_making_tote_bag_macarons_and_glass_of_champagne-p-800_hmyhcq.webp',
       },
       {
         id: '7',
-        img: '/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581192/emily_in_paris_motion_design_exploration/images/Wine_and_cheese_tasting_cutting_the_cheese-p-800_z0ra4m.webp',
       },
       {
         id: '8',
-        img: '/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581178/emily_in_paris_motion_design_exploration/images/Seine_Cruise_Paris_by_Emily_Boat_Logo_-p-800_sf6yws.webp',
       },
       {
         id: '9',
-        img: '/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800.avif',
+        img: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581178/emily_in_paris_motion_design_exploration/images/Seine_Cruise_Captain_and_guest_heart_hands-p-800_oh0buh.webp',
       },
     ],
 
     videoSection: {
-      poster: '/video/paris-hero-b-video.avif',
-      video: '/video/paris-hero-b-video.mp4',
+      poster:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583173/emily_in_paris_motion_design_exploration/videos/paris-hero-b-video_ncqx85.avif',
+      video:
+        'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583188/emily_in_paris_motion_design_exploration/videos/paris-hero-b-video_houykj.mp4',
       marqueeText: 'Discover Experiences',
-      marqueeSticker: '/images/stickers/sticker-montmartre-tour.avif',
+      marqueeSticker:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581404/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_008_c4wmh4.png',
       subSections: {
         main1: {
           title: 'MONTMARTRE TOUR',
-          video: '/video/montmarte-b-transcode.mp4',
-          poster: '/video/montmarte-b-transcode.avif',
+          video:
+            'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583185/emily_in_paris_motion_design_exploration/videos/montmarte-b-transcode_ennz5b.mp4',
+          poster:
+            'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581105/emily_in_paris_motion_design_exploration/images/hero_poster_h1xlbt.avif',
         },
         main2: {
           title: 'CHAMPAGNE SEINE CRUISE',
-          video: '/video/croissants-a-transcode.mp4',
-          poster: '/video/croissants-a-transcode.avif',
+          video:
+            'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583196/emily_in_paris_motion_design_exploration/videos/croissants-a-transcode_ngfkxk.mp4',
+          poster:
+            'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583192/emily_in_paris_motion_design_exploration/videos/croissants-a-transcode_k9bjuv.avif',
         },
 
         main3: {
           title: 'WINE & CHEESE TASTING',
-          video: '/video/wine-tasting-b-transcode.mp4',
-          poster: '/video/wine-tasting-b-transcode.avif',
+          video:
+            'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583191/emily_in_paris_motion_design_exploration/videos/wine-tasting-b-transcode_kuqeul.mp4',
+          poster:
+            'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583192/emily_in_paris_motion_design_exploration/videos/wine-tasting-b-transcode_vrwh3p.avif',
         },
       },
     },
@@ -80,33 +94,39 @@ export const rome = {
     emilyActivities: [
       {
         id: '1',
-        video: '/video/food-tour-a-transcode.mp4',
+        video:
+          'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583186/emily_in_paris_motion_design_exploration/videos/food-tour-a-transcode_xjoufe.mp4',
         type: 'WALKING TOURS',
         title: '<span>Walk in the <br /> footsteps of Emily</span>',
         duration: '2.5HOURS',
         price: 'FROM52€',
         category: 'tour',
-        poster: '/video/food-tour-a-transcode.avif',
+        poster:
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583194/emily_in_paris_motion_design_exploration/videos/food-tour-a-transcode_mgv85m.avif',
       },
       {
         id: '2',
-        video: '/video/croissants-a-transcode.mp4',
+        video:
+          'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583196/emily_in_paris_motion_design_exploration/videos/croissants-a-transcode_ngfkxk.mp4',
         type: 'WORKSHOPS',
         title: '<span>Croissant-Making <br /> Workshop</span>',
         duration: '2.5HOURS',
         price: 'FROM131€',
         category: 'workshop',
-        poster: '/video/croissants-a-transcode.avif',
+        poster:
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583192/emily_in_paris_motion_design_exploration/videos/croissants-a-transcode_k9bjuv.avif',
       },
       {
         id: '3',
-        video: '/video/macarons-a-transcode.mp4',
+        video:
+          'https://res.cloudinary.com/dixqgrowr/video/upload/v1770583213/emily_in_paris_motion_design_exploration/videos/macarons-a-transcode_s8ktcv.mp4',
         type: 'WORKSHOPS',
         title: '<span>Macaron-Making <br /> Workshop</span>',
         duration: '2.5HOURS',
         price: 'FROM131€',
         category: 'workshop',
-        poster: '/video/macarons-a-transcode.avif',
+        poster:
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770583173/emily_in_paris_motion_design_exploration/videos/macarons-a-transcode_iokt0f.avif',
       },
     ],
   },
@@ -115,30 +135,36 @@ export const rome = {
     {
       id: '1',
       textA: 'Walking Tour',
-      stickerA: '/images/stickers/rome/EMIP_Misc25_Icon_007.png',
+      stickerA:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581404/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_007_ojkv26.png',
       textB: 'Food Tour',
-      stickerB: '/images/stickers/rome/EMIP_Misc25_Icon_011.png',
+      stickerB:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581407/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_011_vknggw.png',
     },
     {
       id: '2',
       textA: 'Pizza',
-      stickerA: '/images/stickers/rome/EMIP_Misc25_Icon_008.png',
+      stickerA:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581404/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_008_c4wmh4.png',
       textB: 'Pasta',
-      stickerB: '/images/stickers/rome/EMIP_Misc25_Icon_009.png',
+      stickerB:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581405/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_009_cz8iaj.png',
     },
     {
       id: '3',
       textA: 'APEROL',
-      stickerA: '/images/stickers/rome/EMIP_Misc25_Icon_006.png',
+      stickerA:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581403/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_006_aiifzx.png',
       textB: 'RAVIOLI',
-      stickerB: '/images/stickers/rome/EMIP_Misc25_Icon_010.png',
+      stickerB:
+        'https://res.cloudinary.com/dixqgrowr/image/upload/v1770581406/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_010_dxfpaa.png',
     },
   ],
 
   activities: [
     {
       id: '1',
-      bg: '/images/rome/Walking_Tour_(V2)_-_Croissant_+_Fountain_Portrait.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580845/emily_in_paris_motion_design_exploration/images/rome/Walking_Tour__V2__-_Croissant___Fountain_Portrait_ndfbza.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Walking Tour',
@@ -148,7 +174,7 @@ export const rome = {
     },
     {
       id: '2',
-      bg: '/images/rome/b_Vespa_(V2)_-_Infront_of_colosseum-2.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580839/emily_in_paris_motion_design_exploration/images/rome/b_Vespa__V2__-_Infront_of_colosseum-2_wlju7l.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Vespa Tour',
@@ -158,7 +184,7 @@ export const rome = {
     },
     {
       id: '3',
-      bg: '/images/rome/e_Ravioli_(V2)_-_2_people_making_ravioli.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580839/emily_in_paris_motion_design_exploration/images/rome/e_Ravioli__V2__-_2_people_making_ravioli_qxhvvv.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Ravioli Making with Spritz',
@@ -168,7 +194,7 @@ export const rome = {
     },
     {
       id: '4',
-      bg: '/images/rome/Pasta_(V2)_-_making_pasta.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580841/emily_in_paris_motion_design_exploration/images/rome/Pasta__V2__-_making_pasta_vyksyv.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Pasta and Tiramisu Making',
@@ -178,7 +204,7 @@ export const rome = {
     },
     {
       id: '5',
-      bg: '/images/rome/Food_Tour_(V2)_-_table_of_food.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580840/emily_in_paris_motion_design_exploration/images/rome/Food_Tour__V2__-_table_of_food_djsvft.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Food Tour',
@@ -188,7 +214,7 @@ export const rome = {
     },
     {
       id: '6',
-      bg: '/images/rome/Wine_&_Aperitivo_(V2)_-_wine_infront_of_the_sign.avif',
+      bg: 'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580849/emily_in_paris_motion_design_exploration/images/rome/Wine___Aperitivo__V2__-_wine_infront_of_the_sign_fjjfye.avif',
       leadingSticker: '',
       hoverBg: '',
       title: 'Wine & Aperitivo Tasting',
@@ -200,9 +226,10 @@ export const rome = {
   review: {
     name: 'Amy',
     text: 'Highlight of our Rome trip! So fun getting to go on the back of a vespa with the added Emily in Paris touch!',
-    imageA: '/images/rome/Vespa_(V2)_-_Infront_of_colosseum.avif',
+    imageA:
+      'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580849/emily_in_paris_motion_design_exploration/images/rome/Wine___Aperitivo__V2__-_wine_infront_of_the_sign_fjjfye.avif',
     imageB:
-      '/images/rome/Vespa_Tour_-_butterfly_bag_from_emily_in_paris_vespa_helmet_and_red_vespa_red_hair_tie.avif',
+      'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580849/emily_in_paris_motion_design_exploration/images/rome/Wine___Aperitivo__V2__-_wine_infront_of_the_sign_fjjfye.avif',
 
     reviews: [
       {
@@ -211,7 +238,7 @@ export const rome = {
         name: 'Marcus',
         experience: 'Wine & Aperitivo Tasting',
         image:
-          '/images/rome/Wine - 2 glasses infront of the rome by emily sign.avif',
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580846/emily_in_paris_motion_design_exploration/images/rome/Wine_-_2_glasses_infront_of_the_rome_by_emily_sign_zeypzh.avif',
         location: '',
       },
       {
@@ -219,7 +246,8 @@ export const rome = {
         id: '2',
         name: 'Emma',
         experience: 'Wine & Aperitivo Tasting',
-        image: '/images/rome/Wine - table layout-2.avif',
+        image:
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580849/emily_in_paris_motion_design_exploration/images/rome/Wine_-_table_layout-2_genun2.avif',
         location: '',
       },
       {
@@ -227,7 +255,8 @@ export const rome = {
         id: '3',
         name: 'Matteo',
         experience: 'Wine & Aperitivo Tasting',
-        image: '/images/rome/Wine - cocktails on tray.avif',
+        image:
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580847/emily_in_paris_motion_design_exploration/images/rome/Wine_-_cocktails_on_tray_cbrkxx.avif',
         location: '',
       },
       {
@@ -236,7 +265,7 @@ export const rome = {
         name: 'Wendy',
         experience: 'Walking Tour',
         image:
-          '/images/rome/Walking tour - group of friends sat by fountain.avif',
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580844/emily_in_paris_motion_design_exploration/images/rome/Walking_tour_-_group_of_friends_sat_by_fountain_cjzh1b.avif',
         location: '',
       },
       {
@@ -245,12 +274,12 @@ export const rome = {
         name: 'Stella',
         experience: 'Food Tour',
         image:
-          '/images/rome/Food_Tour_-_Aperol_spritz_and_food_group_of_friends_at_a_table.avif',
+          'https://res.cloudinary.com/dixqgrowr/image/upload/v1770580840/emily_in_paris_motion_design_exploration/images/rome/Food_Tour_-_Aperol_spritz_and_food_group_of_friends_at_a_table_pxzcrn.avif',
         location: '',
       },
     ],
   },
   outro: {
-    text: "<span class='text-h3 flex items-center flex-wrap mx-auto text-(--content-primary)'>From<img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='/images/stickers/rome/EMIP_Misc25_Icon_009.png'/><span class='bg-(--bg-brand-tertiary)'>pasta-making</span>&thinsp; workshops to<img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='/images/stickers/rome/EMIP_Misc25_Icon_006.png'/><span class='bg-(--border-tertiary)'>Vespa rides through Rome</span><span>discover iconic locations,</span><img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='/images/stickers/rome/EMIP_Misc25_Icon_010.png'/><span class='bg-(--bg-brand)'>secret streets</span>&thinsp;<span>and Roman moments à la Emily.</span></span>",
+    text: "<span class='text-h3 flex items-center flex-wrap mx-auto text-(--content-primary)'>From<img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='https://res.cloudinary.com/dixqgrowr/image/upload/v1770581405/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_009_cz8iaj.png'/><span class='bg-(--bg-brand-tertiary)'>pasta-making</span>&thinsp; workshops to<img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='https://res.cloudinary.com/dixqgrowr/image/upload/v1770581403/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_006_aiifzx.png'/><span class='bg-(--border-tertiary)'>Vespa rides through Rome</span><span>discover iconic locations,</span><img loading='lazy' class='mx-2 flex-none object-contain h-[clamp(52.7px,5.4vw+35.4px,102.4px)] w-[clamp(52.7px,5.4vw+35.4px,102.4px)] aspect-square' src='https://res.cloudinary.com/dixqgrowr/image/upload/v1770581406/emily_in_paris_motion_design_exploration/images/stickers/rome/EMIP_Misc25_Icon_010_dxfpaa.png'/><span class='bg-(--bg-brand)'>secret streets</span>&thinsp;<span>and Roman moments à la Emily.</span></span>",
   },
 };
